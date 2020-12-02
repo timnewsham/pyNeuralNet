30a31428ea900e4dd51e22f523bc48fac35aeb80
Tim Newsham <tim.newsham@gmail.com>
2020-12-02 12:30:05 -1000

    add cmd line args, and ability to use neural net for recognition

--- mnist.py
+++ mnist.py
@@ -2,7 +2,7 @@
 """
 MNIST data reader
 """
-import random, struct
+import argparse, random, struct
 import numpy as np
 
 from mynet import Net
@@ -81,17 +81,51 @@ def batchLearn(net, eps, bsz, nb, nl, dat) :
             err = sum(net.err(img, vec[lab]) for lab,img in batch) / nb
             print 'avg batch error', err
 
-def main() :
-    dat = list(readVec('mnist/t10k'))
-    fn = 'mnist.net'
+def getopt() :
+    p = argparse.ArgumentParser(description='digit recognizer')
+    p.add_argument('-t', dest='train', action="store_true", default=False)
+    p.add_argument('-e', dest='eps', type=float, default=0.00001, help="epsilon for training")
+    p.add_argument('-f', dest='nfile', default='mnist.net')
+    opt = p.parse_args()
+    return opt
+
+def train(opt) :
     n = Net(784, 16, 16, 10)
     try :
-        n.load(fn)
+        n.load(opt.nfile)
     except Exception, e :
         print 'cant load', e
 
-    batchLearn(n, 0.001, bsz=100, nb=1000, nl=10, dat=dat)
-    n.save(fn)
+    dat = list(readVec('mnist/t10k'))
+    try :
+        batchLearn(n, opt.eps, bsz=100, nb=1000, nl=10, dat=dat)
+    except KeyboardInterrupt :
+        print "interrupted"
+        opt.nfile += "-int"
+    print "saving", opt.nfile
+    n.save(opt.nfile)
+
+def ident(opt) :
+    net = Net(784, 16, 16, 10)
+    net.load(opt.nfile)
+
+    # just try ten randomly for now
+    dat = list(readVec('mnist/t10k'))
+    random.shuffle(dat)
+    for lab,img in dat[:10] :
+        show(img)
+        best = [(prob,idx) for (idx,prob) in enumerate(net.fwd(img))]
+        best.sort(reverse=True)
+        print "label", lab
+        for p,n in best[:3] :
+            print n, '%%%1.f' % (100.0 * p)
+
+def main() :
+    opt = getopt()
+    if opt.train :
+        train(opt)
+    else :
+        ident(opt)
 
 def test() :
     for lab,arr in readVec('mnist/t10k') :
